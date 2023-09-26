0fe38cbbc8c39eb5f0d2decfb1d62ef1d481eb1d
Maggie Harvey <mharvey82902@gamil.com>
2023-09-26 12:59:03 -0500

    steps 1-5

---
+++ app/src/androidTest/java/model/Pet.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Pet {
+    String name, breed;
+    int age;
+    public Pet(String nameInput, String breedInput, int ageInput){
+        name = nameInput;
+        breed = breedInput;
+        age = ageInput;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
